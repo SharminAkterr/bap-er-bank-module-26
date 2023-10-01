e733bd0dfdf927c2ecb0ae5e01026095d3fda507
SharminAkterr <sharminakter3e@gmail.com>
2023-10-02 01:15:51 +0600

    final

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,29 +1,36 @@
 document.getElementById('btn-withdraw').addEventListener('click', function () {
+    // input part 
     const inputWithdraw = document.getElementById('input-withdraw');
     const inputWithdrawValue = inputWithdraw.value;
     const inputWithdrawString = parseFloat(inputWithdrawValue);
     inputWithdraw.value = '';
 
+    // validation part 
     if (isNaN(inputWithdrawString)) {
         alert('provide valid number');
         return;
     }
+    // how much withdraw show amount will set as number part 
     const newWithdraw = document.getElementById('withdraw-total');
     const newWithdrawInner = newWithdraw.innerText;
     const newWithdrawString = parseFloat(newWithdrawInner);
 
-    if (inputWithdrawString > newWithdrawString) {
-        alert('You have given more than you have.');
-        return;
-    }
-    // adding and where to place this sum 
-    const totalWithdraw = inputWithdrawString + newWithdrawString;
-    newWithdraw.innerText = totalWithdraw;
-
+    // how much withdraw show amount will set as number part
     const currentBalance = document.getElementById('balance-total');
     const currentBalanceInner = currentBalance.innerText;
     const currentBalanceString = parseFloat(currentBalanceInner);
 
+    // validation if withdraw amount is greater than total you have 
+    if (inputWithdrawString > currentBalanceString) {
+        alert('You have given more than you have.');
+        return;
+    }
+
+    // adding and where to set this
+    const totalWithdraw = inputWithdrawString + newWithdrawString;
+    newWithdraw.innerText = totalWithdraw;
+
+    // destructing and where to set this 
     const totalBalance = currentBalanceString - inputWithdrawString;
     currentBalance.innerText = totalBalance
 })
